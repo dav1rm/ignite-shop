14d40d07e6132dac490840f40a096354597f3c64
Davi <davirodrigues8@gmail.com>
2024-04-08 10:34:28 -0300

    add cart provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,27 +8,41 @@ import { Container, Header } from '@/styles/pages/app';
 import CartButton from '@/components/cart-button';
 import CartModal from '@/components/cart-modal';
 import logoImg from '../assets/logo.svg';
+import { CartProvider } from 'use-shopping-cart';
 
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
   const pathname = usePathname();
+  const successUrl = `${process.env.NEXT_URL}/success?session_id={CHECKOUT_SESSION_ID}`;
+  const cancelUrl = `${process.env.NEXT_URL}/`;
 
   return (
-    <Container>
-      <Header>
-        <Image src={logoImg} alt='logo' />
-        <CartButton
-          counter={26}
-          color='dark'
-          size='small'
-          onClick={() => router.push(`${pathname}?modal=true`)}
-        />
-      </Header>
-      <Component {...pageProps} />
-
-      <CartModal />
-    </Container>
+    <CartProvider
+      mode='payment'
+      cartMode='client-only'
+      stripe={process.env.STRIPE_SECRET_KEY as string}
+      successUrl={successUrl}
+      cancelUrl={cancelUrl}
+      currency='BRL'
+      shouldPersist={true}
+      allowedCountries={['BR']}
+      billingAddressCollection={true}
+    >
+      <Container>
+        <Header>
+          <Image src={logoImg} alt='logo' />
+          <CartButton
+            counter={26}
+            color='dark'
+            size='small'
+            onClick={() => router.push(`${pathname}?modal=true`)}
+          />
+        </Header>
+        <Component {...pageProps} />
+        <CartModal />
+      </Container>
+    </CartProvider>
   );
 }
