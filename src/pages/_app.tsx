c61e9b2f5d4838ca091781105ddbec1a16ece2fd
Davi <davirodrigues8@gmail.com>
2024-04-08 10:43:32 -0300

    add cart couter to header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -34,7 +34,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <Header>
           <Image src={logoImg} alt='logo' />
           <CartButton
-            counter={26}
+            showCounter={true}
             color='dark'
             size='small'
             onClick={() => router.push(`${pathname}?modal=true`)}
